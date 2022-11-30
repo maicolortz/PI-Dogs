c2c345badc03927260714c95ffc00e974212e014
Maicol Ortiz Hernandez <maicolortiz28971@gmail.com>
2022-11-29 19:24:37 -0700

    responsive arreglos

--- client/src/components/Landingpage.js
+++ client/src/components/Landingpage.js
@@ -1,15 +1,16 @@
 import React from "react";
-import imagelanding from "../images/port.png";
+import imagelanding from "../images/fondo.webp";
 function Landingpage() {
   return (
-    <div className="landing">
-      <form className="formlanding"method="get" action="/home">
+    <div>
+      <div className="container_img_landing">
+        <img className="imglanding" src={imagelanding} alt="landing page"></img>
+      </div>
+      <form className="formlanding" method="get" action="/home">
         <button className="b-landing" type="submit">
           Home
         </button>
       </form>
-
-      <img className="imglanding" src={imagelanding} alt="landing page"></img>
     </div>
   );
 }
